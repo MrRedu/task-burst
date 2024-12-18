ecb5d9e3180047a80e0f6be208d1dfe011941a4a
Eduardo <ejrb1234@gmail.com>
2024-12-18 17:11:22 -0400

    🤺

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,32 @@
 import type { Metadata } from "next";
-import { Quicksand } from 'next/font/google'
+import { Quicksand } from "next/font/google";
 
 import "./globals.css";
 import { Providers } from "./Providers";
+import { Header, SideBar } from "@/components/.";
 
 const quicksand = Quicksand({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Task Burst',
-    default: 'Task Burst',
+    template: "%s | Task Burst",
+    default: "Task Burst",
   },
-  authors: [{ name: 'Eduardo', url: 'https://github.com/MrRedu' }],
-  creator: 'Mr Redu',
+  authors: [{ name: "Eduardo", url: "https://github.com/MrRedu" }],
+  creator: "Mr Redu",
   description: `Optimize your time with our web app that combines task management, weather forecasting, and the Pomodoro technique. Create to-do lists, set reminders, and check the current weather to plan your day effectively. With an integrated Pomodoro timer, enhance your focus by working in productive intervals. Ideal for students and professionals looking to maximize productivity all in one place.`,
-  keywords: ['Task', 'Pomodoro', 'Planning', 'Weather', 'Task Burst', 'ToDo List'],
-}
+  keywords: [
+    "Task",
+    "Pomodoro",
+    "Planning",
+    "Weather",
+    "Task Burst",
+    "ToDo List",
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -27,11 +35,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${quicksand.className} antialiased h-screen `}
-      >
+      <body className={`${quicksand.className} antialiased h-screen `}>
         <Providers>
-          {children}
+          <div
+            className="
+            grid grid-cols-[auto_1fr] grid-rows-[3rem_1fr] gap-x-2 
+            h-[calc(100vh-8px)]
+            "
+          >
+            <Header className="col-span-2" />
+            <SideBar className="pl-2" />
+            {children}
+          </div>
         </Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,25 @@
-import { SideBar, Header, List, Card, BackgroundDotted } from "./components/";
+import { List, Card, Clock } from "@/components/.";
 
 export default function HomePage() {
   return (
-    <div className="
-    grid grid-cols-[auto_1fr] grid-rows-[3rem_1fr] gap-x-2 
-    h-[calc(100vh-8px)]
-    ">
-      <div className="col-span-2"><Header /></div>
-      <div className="pl-2"><SideBar /></div>
-      <div className="
+    <section
+      className={`
       flex flex-col overflow-y-auto
-      md:grid grid-cols-2 grid-rows-2 gap-2  md:overflow-hidden 
+      md:grid grid-cols-2 grid-rows-2 gap-6  md:overflow-hidden 
       rounded-xl relative mr-2 p-4 z-0
-      ">
-        <Card className="h-full">
-          Weather
-        </Card>
-        <Card className="row-span-2">
-          <List />
-        </Card>
-        <Card className="h-full">
-          Pomodoro
-        </Card>
-        <BackgroundDotted />
-      </div>
-    </div>
+
+      // Background Dotted
+      bg-c-woodsmoke bg-[radial-gradient(#4d4d4d,transparent_1px)] [background-size:24px_24px]
+      `}
+    >
+      <Card className="h-full">Weather</Card>
+      <Card className="row-span-2">
+        <List />
+      </Card>
+      <Card className="h-full">
+        {/* Pomodoro */}
+        <Clock />
+      </Card>
+    </section>
   );
 }
