bb1a2326e34f12e5f200953242d7de8c07599e58
Eduardo <ejrb1234@gmail.com>
2024-12-18 17:01:31 -0400

    Card component

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,7 +1,20 @@
-export const Card = ({ children, className = "" }: { children: React.ReactNode; className?: string }) => {
+interface CardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Card = ({ children, className = "" }: CardProps) => {
   return (
-    <section className={`bg-snow shadow rounded-lg p-4 ${className}`}>
+    <section
+      className={`
+    bg-c-space
+    border-[3px] border-c-dark
+    shadow-[4px_4px_0_0_rgba(5,5,5,1)]
+    text-c-snow
+    
+    rounded-lg p-4 ${className}`}
+    >
       {children}
     </section>
-  )
+  );
 };
