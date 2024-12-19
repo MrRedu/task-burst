b56280d8ccb3e821cc44227f3b54cd478693ba3b
Eduardo <ejrb1234@gmail.com>
2024-12-19 16:04:16 -0400

    Card rounded changed

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -8,11 +8,11 @@ export const Card = ({ children, className = "" }: CardProps) => {
     <section
       className={`
     bg-c-space
+    
     border-[3px] border-c-dark
     shadow-[4px_4px_0_0_rgba(5,5,5,1)]
     text-c-snow
-    
-    rounded-lg p-4 ${className}`}
+    rounded-xl p-4 ${className}`}
     >
       {children}
     </section>
